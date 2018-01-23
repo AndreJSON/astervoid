a8d17f8f7dcf6f7750a6137dcbc21fcbbbde6bf6
AndreJSON <andre.v.json@gmail.com>
2018-01-23 01:31:28 +0100

    Adds movement prototype.

--- public/scripts/game-controller.js
+++ public/scripts/game-controller.js
@@ -11,9 +11,20 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 		if (Date.now() > game.global.nextFrame) {
 			game.global.nextFrame += (1000 / game.global.fps);
 			game.draw();
-			$log.info("Drew background");
+			$log.info("Drew background.");
+		}
+		if (Date.now() > game.global.nextTick) {
+			game.global.nextTick += (1000 / game.global.tps);
+			game.tick();
+			$log.info("Ticked the game.");
 		}
 		requestAnimationFrame(game.loop);
+	};
+
+	game.tick = function () {
+		for (var i = 0; i < game.global.entities.length; i++) {
+			game.global.entities[i].move();
+		}
 	};
 	
 	/**
@@ -26,7 +37,8 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 		}
 	};
 
-	game.drawPart = function (context,x,y,points,color) {
+	game.drawPart = function (x,y,points,color) {
+		var context = $scope.ctx;
 		context.beginPath();
 		context.moveTo(x,y);
 		for (var i = 0; i < points.length; i++) {
@@ -47,38 +59,45 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 		$scope.ctx.fill();
 	};
 
-	game.entity = function (x,y,parts) {
-		this.pos = [x,y];
+	game.entity = function (pos,parts,movement) {
+		this.pos = pos;
 		this.parts = parts;
+		this.movement = movement;
 		this.draw = function () {
 			for (var i = 0; i < this.parts.length; i++) {
 				this.parts[i].draw(this.pos[0], this.pos[1]);
 			}
 		};
+		this.move = function () {
+			pos = movement.nextPos(pos);
+		};
 	};
 
-	game.part = function (x,y,points,color) {
-		this.pos = [x,y];
+	game.part = function (pos,points,color) {
+		this.pos = pos;
 		this.points = points;
 		this.color = color;
 		this.draw = function (x,y) {
-			game.drawPart($scope.ctx,x+this.pos[0],y+this.pos[1],this.points,this.color);
+			game.drawPart(x+this.pos[0],y+this.pos[1],this.points,this.color);
 		}
 	};
+
+	game.movement = function() {
+		this.nextPos = function(pos) {
+			return pos;
+		};
+	}
 	
 	//Keeps track of global game stuff.
 	game.global = {
 		fps: 30,
+		tps: 3,
 		nextFrame: undefined,
-		colors: {
-			background: "rgba(30,30,30,1)",
-			ship1: "rgba(140,150,160,1)",
-			ship2: "rgba(140,130,130,1)",
-			ship3: "rgba(100,90,90,1)"
-		},
-		entities: [],
+		nextTick: undefined,
+		colors: gameFactory.colors,
 		parts: gameFactory.parts,
-		utils: gameFactory.utils
+		utils: gameFactory.utils,
+		entities: []
 	};
 	
 	/**
@@ -87,13 +106,17 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 	game.init = function () {
 		game.loop();
 		game.global.nextFrame = Date.now();
-		game.global.entities.push(new game.entity(90,420,[
-			new game.part(-50,-10,game.global.parts.nozzle1,game.global.colors.ship3),
-			new game.part(-50, 26,game.global.parts.nozzle1,game.global.colors.ship3),
-			new game.part(0,-30,game.global.parts.gun1,game.global.colors.ship2),
-			new game.part(0,30,game.global.utils.mirrorX(game.global.parts.gun1),game.global.colors.ship2),
-			new game.part(-50,-30,game.global.parts.body1,game.global.colors.ship1)
-		]));
+		game.global.nextTick = Date.now();
+		game.global.entities.push(new game.entity([90,420],[
+			new game.part([-50,-10],game.global.parts.nozzle1,game.global.colors.ship3),
+			new game.part([-50, 26],game.global.parts.nozzle1,game.global.colors.ship3),
+			new game.part([0,-30],game.global.parts.gun1,game.global.colors.ship2),
+			new game.part([0,30],game.global.utils.mirrorX(game.global.parts.gun1),game.global.colors.ship2),
+			new game.part([-50,-30],game.global.parts.body1,game.global.colors.ship1)
+		],new game.movement()));
+		game.global.entities.push(new game.entity([500,500],[
+			new game.part([0,-20],game.global.utils.scale(game.global.parts.star1,3,3),game.global.colors.star1)
+		],new game.movement()));
 	};
 	
 	/**
--- public/scripts/game-factory.js
+++ public/scripts/game-factory.js
@@ -3,13 +3,28 @@ angular.module('app').factory('gameFactory', function () {
 		parts: {
 			gun1: [[0,0], [0,-3], [-10,-3], [-10,-15], [45,-15], [45,-3], [25,-3], [25,0], [0,0]],
 			nozzle1: [[0,0], [-10,4], [-10,-20], [0,-16], [0,0]],
-			body1: [[0,0], [100,0], [100,60], [0,60], [0,0]]
+			body1: [[0,0], [100,0], [100,60], [0,60], [0,0]],
+			star1: [[0,0],[1,2],[3,3],[1,4],[0,6],[-1,4],[-3,3],[-1,2],[0,0]]
+		},
+		colors: {
+			background: "rgba(30,30,30,1)",
+			ship1: "rgba(140,150,160,1)",
+			ship2: "rgba(140,130,130,1)",
+			ship3: "rgba(100,90,90,1)",
+			star1: "rgba(255,255,255,0.8)"
 		},
 		utils: {
 			mirrorX: function (points) {
 				res = [];
 				for (var i = 0; i < points.length; i++) {
 					res.push([points[i][0],-1*points[i][1]]);
+				}
+				return res;
+			},
+			scale: function (points,changeX, changeY) {
+				res = [];
+				for (var i = 0; i < points.length; i++) {
+					res.push([changeX*points[i][0],changeY*points[i][1]]);
 				}
 				return res;
 			}
