493ea251dc75bb1c22a935b3f10a33852acea5f9
AndreJSON <andre.v.json@gmail.com>
2018-01-23 10:57:22 +0100

    Divided draw and tick loop.

--- public/scripts/game-controller.js
+++ public/scripts/game-controller.js
@@ -7,18 +7,20 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 	/**
 	 * Loops over all functions needed to progress the game.
 	 */
-	game.loop = function () {
-		if (Date.now() > game.global.nextFrame) {
-			game.global.nextFrame += (1000 / game.global.fps);
-			game.draw();
-			$log.info("Drew background.");
-		}
+	game.tickLoop = function () {
 		if (Date.now() > game.global.nextTick) {
 			game.global.nextTick += (1000 / game.global.tps);
 			game.tick();
 			$log.info("Ticked the game.");
 		}
-		requestAnimationFrame(game.loop);
+		$timeout(game.tickLoop, 10);
+	};
+
+	game.drawLoop = function () {
+		game.global.nextFrame = Date.now() + (1000 / game.global.fps);
+		game.draw($scope.ctx);
+		$log.info("Drew background.");
+		requestAnimationFrame(game.drawLoop);
 	};
 
 	game.tick = function () {
@@ -30,42 +32,23 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 	/**
 	 * Redraws the screen and all its entities.
 	 */
-	game.draw = function () {
-		game.drawBackground();
-		for (var i = 0; i < game.global.entities.length; i++) {
-			game.global.entities[i].draw();
-		}
-	};
-
-	game.drawPart = function (x,y,points,color) {
-		var context = $scope.ctx;
+	game.draw = function (context) {
 		context.beginPath();
-		context.moveTo(x,y);
-		for (var i = 0; i < points.length; i++) {
-			context.lineTo(x+points[i][0],y+points[i][1]);
-		}
-		context.closePath();
-		context.fillStyle = color;
+		context.rect(0, 0, game.global.windowWidth, game.global.windowHeight);
+		context.fillStyle = game.global.colors.background;
 		context.fill();
-	}
-	
-	/**
-	 * Subroutine to draw. Draws the background.
-	 */
-	game.drawBackground = function () {
-		$scope.ctx.beginPath();
-		$scope.ctx.rect(0, 0, game.global.windowWidth, game.global.windowHeight);
-		$scope.ctx.fillStyle = game.global.colors.background;
-		$scope.ctx.fill();
+		for (var i = 0; i < game.global.entities.length; i++) {
+			game.global.entities[i].draw(context);
+		}
 	};
 
 	game.entity = function (pos,parts,movement) {
 		this.pos = pos;
 		this.parts = parts;
 		this.movement = movement;
-		this.draw = function () {
+		this.draw = function (context) {
 			for (var i = 0; i < this.parts.length; i++) {
-				this.parts[i].draw(this.pos[0], this.pos[1]);
+				this.parts[i].draw(context, this.pos[0], this.pos[1]);
 			}
 		};
 		this.move = function () {
@@ -77,22 +60,36 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 		this.pos = pos;
 		this.points = points;
 		this.color = color;
-		this.draw = function (x,y) {
-			game.drawPart(x+this.pos[0],y+this.pos[1],this.points,this.color);
-		}
+		this.draw = function (context,x,y) {
+			x += this.pos[0];
+			y += this.pos[1];
+			context.beginPath();
+			context.moveTo(x,y);
+			for (var i = 0; i < this.points.length; i++) {
+				context.lineTo(x+this.points[i][0],y+this.points[i][1]);
+			}
+			context.closePath();
+			context.fillStyle = this.color;
+			context.fill();
+		};
 	};
 
 	game.movement = function() {
 		this.nextPos = function(pos) {
 			return pos;
 		};
-	}
+	};
+
+	game.createStar = function () {
+		var pos = [500,500];
+		var body = new game.part([0,-20],game.global.utils.scale(game.global.parts.star1,3,3),game.global.colors.star1);
+		var movement = new game.movement();
+		return new game.entity(pos, [body], movement);
+	};
 	
 	//Keeps track of global game stuff.
 	game.global = {
-		fps: 30,
-		tps: 3,
-		nextFrame: undefined,
+		tps: 20,
 		nextTick: undefined,
 		colors: gameFactory.colors,
 		parts: gameFactory.parts,
@@ -104,8 +101,6 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 	 * Should be called to initialize the game.
 	 */
 	game.init = function () {
-		game.loop();
-		game.global.nextFrame = Date.now();
 		game.global.nextTick = Date.now();
 		game.global.entities.push(new game.entity([90,420],[
 			new game.part([-50,-10],game.global.parts.nozzle1,game.global.colors.ship3),
@@ -114,9 +109,9 @@ angular.module('app').controller('gameController', function ($scope, $log, $time
 			new game.part([0,30],game.global.utils.mirrorX(game.global.parts.gun1),game.global.colors.ship2),
 			new game.part([-50,-30],game.global.parts.body1,game.global.colors.ship1)
 		],new game.movement()));
-		game.global.entities.push(new game.entity([500,500],[
-			new game.part([0,-20],game.global.utils.scale(game.global.parts.star1,3,3),game.global.colors.star1)
-		],new game.movement()));
+		game.global.entities.push(game.createStar());
+		game.drawLoop();
+		game.tickLoop();
 	};
 	
 	/**
